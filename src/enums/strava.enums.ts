ac2d3b8a232eb1d9c1d911378a5966c8fa643f61
Soumya Banerjee <banerjeesoumya771@gmail.com>
2026-05-18 21:11:07 +0530

    updated activity metrics calculation

---
+++ src/enums/strava.enums.ts
@@ -0,0 +1,17 @@
+export enum ActivityMetricAccuracy {
+  HIGH = 'high',
+  MEDIUM = 'medium',
+  LOW = 'low',
+  VERY_LOW = 'very_low',
+  NONE = 'none',
+}
+
+export enum ActivityMetricSource {
+  HEART_RATE = 'heart_rate',
+  POWER = 'power',
+  KILOJOULES = 'kilojoules',
+  CALORIES = 'calories',
+  COMPOSITE = 'composite',
+  TIME = 'time',
+  NONE = 'none',
+}
